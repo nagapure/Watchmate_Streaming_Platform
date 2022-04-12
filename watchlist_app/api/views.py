57e7ee25ab56af3f50e2ccc6628ba9cbb22e78be
Chetan <nagapure50@gmail.com>
2022-04-12 13:55:43 +0530

    Added : Login

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import get_object_or_404
 from watchlist_app.models import Review, StreamPlatform, Watchlist
 from watchlist_app.api.serializers import ReviewSerializer, StreamPlatformSerializer, WatchlistSerializer
@@ -8,6 +9,7 @@ from rest_framework import status
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError 
 
 
 # Generics
@@ -29,11 +31,21 @@ class ReviewList(generics.ListAPIView):
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
     
+    def get_queryset(self):
+        return Review.objects.all()
+     
+    
     def perform_create(self,serializer):
         pk = self.kwargs.get('pk')
-        movie = Watchlist.objects.get(pk=pk)
+        watchlist = Watchlist.objects.get(pk=pk)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user = review_user)
         
-        serializer.save(watchlist = movie)
+        if review_queryset.exists():
+            raise ValidationError("You have already posted review for this movie.")
+        
+        
+        serializer.save(watchlist = watchlist, review_user = review_user)
         
     
     
