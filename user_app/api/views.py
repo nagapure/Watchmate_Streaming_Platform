b8128b6831476b495be351ad8837ad2d995e5abd
Chetan <nagapure50@gmail.com>
2022-04-26 14:57:27 +0530

    Added user Registration using Simple Jwt

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
-from user_app import models
+# from user_app import models
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Logout view for user
@@ -32,8 +33,15 @@ def registration_view(request):
             data['username'] = account.username
             data['email'] = account.email
             
-            token = Token.objects.get(user = account).key
-            data['token'] = token
+            # token = Token.objects.get(user = account).key
+            # data['token'] = token
+            
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                                'refresh': str(refresh),
+                                'access': str(refresh.access_token),
+                            }
+          
         
         else:
             data = serializer.errors
