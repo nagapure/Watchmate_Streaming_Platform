90cd0d3079dc44259d4bf4011a390985aa080ab4
Chetan <nagapure50@gmail.com>
2022-04-26 19:34:22 +0530

    Updated code with formatting

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -9,8 +9,6 @@ from watchlist_app.api.views import (
     ReviewCreate,
     WatchlistListAV,
     WatchlistDetailsAV,
-    # StreamPlatformAV,
-    # StreamPlatformDetailAV
 )
 
 router = DefaultRouter()
@@ -21,15 +19,7 @@ urlpatterns = [
     path("list/", WatchlistListAV.as_view(), name="movie-list"),
     path("<int:pk>/", WatchlistDetailsAV.as_view(), name="movie-details"),
     path("", include(router.urls)),
-    # path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
-    # path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-details'),
-    # This review urls for model Mixin
-    # path('review/', ReviewList.as_view(), name='review-list'),
-    # path('review/<int:pk>', ReviewDetails.as_view(), name='review-details'),
     path("<int:pk>/review-create/", ReviewCreate.as_view(), name="review-create"),
     path("<int:pk>/review/", ReviewList.as_view(), name="review-list"),
     path("review/<int:pk>/", ReviewDetails.as_view(), name="review-details"),
-    # This review urls for APIView class
-    # path('review/', ReviewListAV.as_view(), name='review-list'),
-    # path('review/<int:pk>', ReviewDetailsAV.as_view(), name='review-details'),
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -72,9 +72,7 @@ class ReviewList(generics.ListAPIView):
         query = Review.objects.filter(watchlist=pk)
         return query
 
-    # def watchlist_data(self,request):
-    #     data =
-
+    
 
 
 class ReviewDetails(generics.RetrieveUpdateDestroyAPIView):
@@ -84,27 +82,6 @@ class ReviewDetails(generics.RetrieveUpdateDestroyAPIView):
     # Customer permissions
     permission_classes = [IsReviewUserOrReadOnly]
     throttle_classes = [UserRateThrottle, AnonRateThrottle]
-
-
-# class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-#     queryset = Review.objects.all()
-#     serializer_class = ReviewSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
-
-
-# class ReviewDetails(mixins.RetrieveModelMixin, generics.GenericAPIView):
-#     queryset = Review.objects.all()
-#     serializer_class = ReviewSerializer
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-
-
-# View for Watchlist ==============================
 
 
 class WatchlistListAV(APIView):
@@ -176,44 +153,6 @@ class SteamPlatformVS(viewsets.ModelViewSet):
     serializer_class = StreamPlatformSerializer
 
 
-# ViewSet for SteamPlatform ==============================
-
-# class SteamPlatformVS(viewsets.ViewSet):
-#     def list(self, request):
-#         queryset = StreamPlatform.objects.all()
-#         serializer = StreamPlatformSerializer(queryset, many=True)
-#         return Response(serializer.data)
-
-#     def retrieve(self, request, pk=None):
-#         queryset = StreamPlatform.objects.all()
-#         watchlist = get_object_or_404(queryset, pk=pk)
-#         serializer = StreamPlatformSerializer(watchlist)
-#         return Response(serializer.data)
-
-
-#     def create(self, request):
-#         serializer = StreamPlatformSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status = status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-
-#     def delete(self, request, pk):
-#         try:
-#             platform = StreamPlatform.objects.get(pk=pk)
-#             platform.delete()
-#             return Response({
-#                 'message': 'Platform deleted'
-#             }, status = status.HTTP_204_NO_CONTENT)
-#         except StreamPlatform.DoesNotExist:
-#             return Response({
-#                 'error': 'Platform not found'
-#             }, status = status.HTTP_404_NOT_FOUND)
-
-
-# View for SteamPlatform ==============================
-
 
 class StreamPlatformAV(APIView):
     permission_classes = [IsAdminOrReadOnly]
@@ -268,99 +207,5 @@ class StreamPlatformDetailAV(APIView):
             )
 
 
-# View for Review using APIView ==============================
 
 
-class _ReviewListAV(APIView):
-    def get(self, request):
-        review = Review.objects.all()
-        serializer = ReviewSerializer(review, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ReviewSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-
-class _ReviewDetailsAV(APIView):
-    def get(self, request, pk):
-        try:
-            review = Review.objects.get(pk=pk)
-        except Review.DoesNotExist:
-            return Response({"message": "Review not found"})
-
-        serializer = ReviewSerializer(review)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk):
-        try:
-            review = Review.objects.get(pk=pk)
-        except Review.DoesNotExist:
-            return Response({"message": "Review not found"})
-        serializer = ReviewSerializer(review, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk):
-        try:
-            review = Review.objects.get(pk=pk)
-            review.delete()
-            return Response(
-                {"message": "Review deleted"}, status=status.HTTP_204_NO_CONTENT
-            )
-        except Review.DoesNotFound:
-            return Response(
-                {"message": "Review not found"}, status=status.HTTP_404_NOT_FOUND
-            )
-
-
-# @api_view(['GET', 'POST'])
-# def movie_list(request):
-#     if(request.method == 'GET'):
-#         movies = Movie.objects.all()
-#         # print(movies)
-#         serializer = MovieSerializer(movies, many=True)
-#         return Response(serializer.data)
-
-#     if(request.method == 'POST'):
-#         serializer = MovieSerializer(data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors)
-
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def movie_details(request, pk):
-#     if(request.method == 'GET'):
-#         try:
-#             movie = Movie.objects.get(pk=pk)
-#         except Movie.DoesNotExist:
-#             return Response({
-#                 "Error":"Movie does not exist"
-#             },status=status.HTTP_404_NOT_FOUND
-#            )
-#         serializer = MovieSerializer(movie)
-#         return Response(serializer.data)
-
-#     if(request.method == 'PUT'):
-#         movie = Movie.objects.get(pk=pk)
-#         serializer = MovieSerializer(movie, data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors)
-
-#     if(request.method == 'DELETE'):
-#         movie = Movie.objects.get(pk=pk)
-#         movie.delete()
-#         return Response({
-#             'mesg':'This data is deleted'
-#         }, status = status.HTTP_204_NO_CONTENT)
