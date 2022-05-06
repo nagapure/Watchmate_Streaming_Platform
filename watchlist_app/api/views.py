da965045894abb78afaaccfb19c13615ffad8557
Chetan <nagapure50@gmail.com>
2022-05-06 18:23:38 +0530

    Added Ordering

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -119,8 +119,13 @@ class WatchlistAV(generics.ListAPIView):
     
     
     # SearchFilter
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'platform__name']
+    # filter_backends = [filters.SearchFilter]
+    # search_fields = ['title', 'platform__name']
+    
+    
+    # OrderingFilter
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['avg_rating']
 
 
 class WatchlistListAV(APIView):
--- watchmate/settings.py
+++ watchmate/settings.py
@@ -138,4 +138,7 @@ REST_FRAMEWORK = {
         "review-create": "2/day",
         "review-list": "100/day",
     },
+    
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 5,
 }
