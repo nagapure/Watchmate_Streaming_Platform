a64520b97e9e8d840d0b2a974d957001b1882562
Chetan <nagapure50@gmail.com>
2022-05-22 20:29:10 +0530

    Updated : custom pagination

--- watchlist_app/api/pagination.py
+++ watchlist_app/api/pagination.py
@@ -1,6 +1,10 @@
 from rest_framework.pagination import PageNumberPagination
 
 class WatchlistPagination(PageNumberPagination):
-    page_size = 7
+    page_size = 5
     page_query_param =  'p'
+    # it use to go give an access to client to load data as per their requirement
     page_size_query_param = 'size'
+    max_page_size = 10
+    last_page_strings = 'end'
+    
